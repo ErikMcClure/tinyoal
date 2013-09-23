c52b127ab8b244136541e388ad6eee2d0941a0f7
Erik McClure <erikm@blackspherestudios.com>
2013-09-23 13:13:33 +0000

    makefile commit from POSIX

---
+++ 04mp3file.mk
@@ -0,0 +1,20 @@
+TARGET := 04Mp3File
+SRCDIR := examples/04Mp3File
+BUILDDIR := examples/bin
+OBJDIR := examples/bin/obj
+C_SRCS := $(wildcard $(SRCDIR)/*.c)
+CXX_SRCS := $(wildcard $(SRCDIR)/*.cpp)
+INCLUDE_DIRS := include
+LIBRARY_DIRS := 
+LIBRARIES := dl tinyoal
+
+CPPFLAGS += -w -std=gnu++0x
+LDFLAGS += -L./bin/
+
+include base.mk
+
+distclean:
+	@- $(RM) $(OBJS)
+	@- $(RM) -r $(OBJDIR)
+	@- $(RM) bin/*.txt
+	@- $(RM) bin/*.ini
--- examples/06Managed/Properties/AssemblyInfo.cs
+++ examples/06Managed/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("05Managed")]
+[assembly: AssemblyTitle("06Managed")]
 [assembly: AssemblyDescription("Example for TinyOAL")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Black Sphere Studios")]
